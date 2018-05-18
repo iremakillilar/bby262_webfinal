f9522e2ca539291c2251e2dd61a7f0391add83f5
İrem Sıla Akıllılar <32552162+iremakillilar@users.noreply.github.com>
2018-05-18 11:49:26 +0300

    Add files via upload

---
+++ web_odev02/js/var.js
@@ -0,0 +1,64 @@
+var mymenu=
+'<div class="collapse navbar-collapse" id="navbarCollapse">'+
+'<ul class="navbar-nav mr-auto">'+
+'<li class="nav-item active">'+
+'<a class="nav-link" href="index.html">Ana Sayfa <span class="sr-only">(current)</span></a>'+
+'</li>'+
+'<li class="nav-item">'+
+'<a class="nav-link" href="nedir.html">Nedir?</a>'+
+'</li>'+
+'<li class="nav-item">'+
+'<a class="nav-link" href="nasil.html">Nasıl Yapılır?</a>'+
+'</li>'+
+'<li class="nav-item">'+
+'<a class="nav-link" href="seminer.html">Seminerler</a>'+
+'</li>'+ 
+'<li class="nav-item">'+
+'<a class="nav-link" href="video.html">Video Serisi</a>'+
+'</li>'+
+'<li class="nav-item">'+
+'<a class="nav-link" href="iletisim.html">İletişim</a>'+
+'</li>'+    
+'</ul>'+
+
+'</div>' ;
+
+var ayak=
+'<footer class="container">'+
+'<p class="float-right"><a href="#">Başa Dön</a></p>'+
+'<p>&copy; 2017-2018 Dijitalleştirme Platformu &middot; <a href="#"></a> &middot; <a href="#"></a></p>'+
+'</footer>' ;
+
+var baslik=
+'<div class="carousel-inner">'+
+'<div class="carousel-item active">'+
+'<img class="first-slide" src="img/arka.jpg" alt="First slide">'+
+'<div class="container">'+
+'<div class="carousel-caption text-left">'+
+'<h1>Dijitalleştirme Hakkında Her şey</h1>'+
+'<p>Türkiyenin en güncel dijitalleştirme platformu.Haberlerden haberdar olmak için okumaya devam edin.</p>'+
+ '<p><a class="btn btn-lg btn-primary" href="nedir.html" role="button">Devam Et</a></p>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'<div class="carousel-item">'+
+'<img class="second-slide" src="img/arka.jpg" alt="Second slide">'+
+'<div class="container">'+
+'<div class="carousel-caption">'+
+'<h1>Seminerler</h1>'+
+'<p>I. Uluslarası Dijitalleştirme Konferansı 5 Haziran 2018 tarihinde yapılacaktır.</p>'+
+'<p><a class="btn btn-lg btn-primary" href="#" role="button">Daha Fazla</a></p>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'<div class="carousel-item">'+
+'<img class="third-slide" src="img/arka.jpg" alt="Third slide">'+
+'<div class="container">'+
+'<div class="carousel-caption text-right">'+
+'<h1>Atölyeler</h1>'+
+'<p>Şehrinize gelelim, dijitalleştirmenin nasıl yapıldığını beraber öğrenelim.</p>'+
+'<p><a class="btn btn-lg btn-primary" href="#" role="button">Daha Fazla</a></p>'+
+'</div>'+
+'</div>'+
+'</div>'+
+'</div>';
